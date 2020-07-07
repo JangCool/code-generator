3f306a799461e0e4d36796f67d462aead0d3164c
장진철(zerocooldog@pionnet.co.kr) <zerocooldog@pionnet.co.kr>
2020-07-07 12:59:49 +0900

    로그변경.

--- src/main/java/code/generator/common/Log.java
+++ src/main/java/code/generator/common/Log.java
@@ -14,15 +14,28 @@ public class Log {
 	public static void debug(String message) {
 		out.println("DEBUG : ".concat(message));
 	}
+	
+	public static void debug(Object object) {
+		debug((object != null) ? object.toString() : "null");
+	}
+	
 	public static void debug(Short message) {
 		out.println("DEBUG : ".concat(message.toString()));
 	}
 	public static void warning(String message) {
 		out.println("WARNING : ".concat(message));
 	}
+	
+	public static void warning(Object object) {
+		warning((object != null) ? object.toString() : "null");
+	}
 
 	public static void info(String message) {
 		out.println("INFO : ".concat(message));
+	}
+	
+	public static void info(Object object) {
+		info((object != null) ? object.toString() : "null");
 	}
 	
 	public static void error(String message) {
